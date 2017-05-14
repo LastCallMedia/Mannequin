4b0cc0408ec69b716a00f5036646b9b1b12dda22
Rob Bayliss <rob@lastcallmedia.com>
2017-05-14 11:04:15 -0400

    Beef up configuration tests

--- core/Tests/ConfigTest.php
+++ core/Tests/ConfigTest.php
@@ -5,17 +5,65 @@ namespace LastCall\Patterns\Core\Tests;
 
 
 use LastCall\Patterns\Core\Config;
+use LastCall\Patterns\Core\ConfigInterface;
 use LastCall\Patterns\Core\Discovery\ChainDiscovery;
+use LastCall\Patterns\Core\Discovery\DiscoveryInterface;
 use LastCall\Patterns\Core\Extension\CoreExtension;
+use LastCall\Patterns\Core\Extension\ExtensionInterface;
+use LastCall\Patterns\Core\Pattern\PatternCollection;
 use LastCall\Patterns\Core\Render\DelegatingRenderer;
+use LastCall\Patterns\Core\Variable\ScalarFactory;
 use LastCall\Patterns\Core\Variable\VariableFactory;
+use LastCall\Patterns\Core\Variable\VariableFactoryInterface;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 
 class ConfigTest extends TestCase {
+
+  public function testSetsConfigWhenExtensionsAreUsed() {
+    $extension = $this->prophesize(ExtensionInterface::class);
+    $config = new Config();
+    $extension->setConfig($config)->shouldBeCalled();
+    $config->addExtension($extension->reveal());
+    $config->getExtensions();
+  }
 
   public function testHasDiscovery() {
     $config = new Config();
     $this->assertInstanceOf(ChainDiscovery::class, $config['discovery']);
+  }
+
+  private function getMockExtension() {
+    $extension = $this->prophesize(ExtensionInterface::class);
+    $extension->setConfig(Argument::type(ConfigInterface::class))->will(function() {});
+    return $extension;
+  }
+
+  public function testUsesExtensionDiscoverers() {
+    $discoverer = $this->prophesize(DiscoveryInterface::class);
+    $discoverer->discover()
+      ->willReturn(new PatternCollection())
+      ->shouldBeCalled();
+    $extension = $this->getMockExtension();
+    $extension->getDiscoverers()->willReturn([$discoverer]);
+    $config = new Config();
+    $config->addExtension($extension->reveal());
+    $config->getCollection();
+  }
+
+  public function testUsesExtensionVariableFactories() {
+    $factory = $this->prophesize(VariableFactoryInterface::class);
+    $factory->getTypes()->willReturn(['foo']);
+
+    $extension = $this->getMockExtension();
+    $extension->getVariableFactories()
+      ->willReturn([$factory])
+      ->shouldBeCalled();
+
+    $extension->getVariableFactories()->shouldBeCalled();
+    $config = new Config();
+    $config->addExtension($extension->reveal());
+    $this->assertTrue($config->getVariableFactory()->hasType('foo'));
   }
 
   public function testHasCoreExtension() {
