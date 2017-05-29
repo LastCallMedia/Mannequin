8892341d6a48a2803a1bc2451cab8d626e98ebbc
Rob Bayliss <rob@lastcallmedia.com>
2017-05-29 15:53:58 -0400

    Slim down TwigExtension

--- twig/Extension/TwigExtension.php
+++ twig/Extension/TwigExtension.php
@@ -16,61 +16,29 @@ use LastCall\Mannequin\Twig\Subscriber\InlineTwigYamlMetadataSubscriber;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Finder\Finder;
 
-/**
- * @method addExtension(\Twig_ExtensionInterface $extension)
- * @method addFilter(\Twig_Filter $filter)
- * @method addFunction(\Twig_Function $function)
- * @method addTest(\Twig_Test $test)
- */
 class TwigExtension extends AbstractExtension {
-
-  protected static $proxiedMethods = [
-    'addExtension',
-    'addFilter',
-    'addFunction',
-    'addTest',
-  ];
-
 
   public function __construct(array $config = []) {
     $config += [
       'paths' => [],
+      'twig' => function() {
+        $cache_dir = $this->getConfig()->getCacheDir().DIRECTORY_SEPARATOR.'twig';
+        $loader = new \Twig_Loader_Filesystem($this['paths']);
+        return new \Twig_Environment($loader, [
+          'cache' => $cache_dir,
+          'auto_reload' => TRUE,
+        ]);
+      },
+      'finder' => function() {
+        return Finder::create()
+          ->files()
+          ->in($this['paths']);
+      }
     ];
     parent::__construct($config);
-    $this['finder'] = function() {
-      return new Finder();
-    };
-    $this['loader'] = function() {
-      return new \Twig_Loader_Filesystem($this['paths']);
-    };
-    $this['twig'] = function() {
-      $cache_dir = $this->getConfig()->getCacheDir().DIRECTORY_SEPARATOR.'twig';
-      return new \Twig_Environment($this['loader'], [
-        'cache' => $cache_dir,
-        'auto_reload' => TRUE,
-      ]);
-    };
     $this['discovery'] = function() {
       return new TwigFileDiscovery($this['twig']->getLoader(), $this['finder'], $this->getConfig()->getDispatcher());
     };
-  }
-
-  public function in($dirs) {
-    return $this->extend('finder', function(Finder $finder) use ($dirs) {
-      $finder->in($dirs);
-      return $finder;
-    });
-    return $this;
-  }
-
-  public function __call($name, $arguments) {
-    if(in_array($name, static::$proxiedMethods)) {
-      $this->extend('twig', function(\Twig_Environment $twig) use ($name, $arguments) {
-        $twig->{$name}(...$arguments);
-        return $twig;
-      });
-      return TRUE;
-    }
   }
 
   public function getDiscoverers(): array {
