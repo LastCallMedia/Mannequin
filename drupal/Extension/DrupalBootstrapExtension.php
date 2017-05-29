d9c59ba460c6587234d1971ff735e50462b46df8
Rob Bayliss <rob@lastcallmedia.com>
2017-05-29 15:53:44 -0400

    Move drupal boot into its own method

--- drupal/Extension/DrupalBootstrapExtension.php
+++ drupal/Extension/DrupalBootstrapExtension.php
@@ -14,29 +14,39 @@ class DrupalBootstrapExtension extends TwigExtension {
     $config += [
       'drupal_root' => NULL,
       'extensions' => [],
+      'drupal' => function() {
+        return $this->bootDrupal();
+      },
     ];
     parent::__construct($config);
 
-    $this['drupal'] = function() {
-      chdir($this['drupal_root']);
-      $autoloader = require $this['drupal_root'].'/autoload.php';
-      require_once $this['drupal_root'] . '/core/includes/bootstrap.inc';
-
-      $request = Request::create('/', 'GET', [], [], [], ['SCRIPT_NAME' => $this['drupal_root'] . '/index.php']);
-      $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod', FALSE);
-      Settings::initialize($this['drupal_root'], DrupalKernel::findSitePath($request), $autoloader);
-      $kernel->boot();
-      $kernel->preHandle($request);
-
-      return $kernel->getContainer();
+    $this['_drupal_root'] = function() {
+      if(is_dir($this['drupal_root'])) {
+        return $this['drupal_root'];
+      }
+      throw new \InvalidArgumentException(sprintf('Invalid Drupal Root: %s', $this['drupal_root']));
     };
     $this['twig'] = function() {
       return $this['drupal']->get('twig');
     };
-
     $this['discovery'] = function() {
-      return new DrupalExtensionTwigDiscovery($this['drupal_root'], $this['extensions'], $this['twig']->getLoader(), $this->getConfig()->getDispatcher());
+      return new DrupalExtensionTwigDiscovery($this['_drupal_root'], $this['extensions'], $this['twig']->getLoader(), $this->getConfig()->getDispatcher());
     };
+  }
+
+  protected function bootDrupal() {
+    $drupal_root = $this['_drupal_root'];
+    chdir($drupal_root);
+    $autoloader = require $drupal_root.'/autoload.php';
+    require_once $drupal_root . '/core/includes/bootstrap.inc';
+
+    $request = Request::create('/', 'GET', [], [], [], ['SCRIPT_NAME' => $drupal_root . '/index.php']);
+    $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod', FALSE);
+    Settings::initialize($drupal_root, DrupalKernel::findSitePath($request), $autoloader);
+    $kernel->boot();
+    $kernel->preHandle($request);
+
+    return $kernel->getContainer();
   }
 
   public function getDiscoverers(): array {
