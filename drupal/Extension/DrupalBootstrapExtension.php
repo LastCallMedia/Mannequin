a19f6f1510e450ed62a45d475bc570c612ad7148
Rob Bayliss <rob@lastcallmedia.com>
2017-06-03 17:01:12 -0400

    Simplify TwigDiscovery

--- drupal/Extension/DrupalBootstrapExtension.php
+++ drupal/Extension/DrupalBootstrapExtension.php
@@ -6,6 +6,8 @@ use Drupal\Core\DrupalKernel;
 use Drupal\Core\Site\Settings;
 use LastCall\Mannequin\Drupal\Discovery\DrupalExtensionTwigDiscovery;
 use LastCall\Mannequin\Twig\Extension\TwigExtension;
+use LastCall\Mannequin\Twig\TemplateFilenameIterator;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Request;
 
 class DrupalBootstrapExtension extends TwigExtension {
@@ -18,6 +20,25 @@ class DrupalBootstrapExtension extends TwigExtension {
       'drupal' => function() {
         return $this->bootDrupal();
       },
+      'finder' => function() {
+        $finder = Finder::create()
+          ->name('*.html.twig')
+          ->files();
+        foreach($this['extension_paths'] as $extension_path) {
+          $finder->in($extension_path);
+        }
+        return $finder;
+      },
+      'names' => function() {
+        $loader = $this['drupal']->get('twig.loader.filesystem');
+        $iterator = new TemplateFilenameIterator($this['finder']);
+        foreach($loader->getNamespaces() as $namespace) {
+          foreach($loader->getPaths($namespace) as $path) {
+            $iterator->addPath(sprintf('%s/%s', $this['drupal_root'], $path), $namespace);
+          }
+        }
+        return $iterator;
+      }
     ];
     parent::__construct($config);
 
@@ -30,8 +51,13 @@ class DrupalBootstrapExtension extends TwigExtension {
     $this['twig'] = function() {
       return $this['drupal']->get('twig');
     };
-    $this['discovery'] = function() {
-      return new DrupalExtensionTwigDiscovery($this['_drupal_root'], $this['extensions'], $this['twig']->getLoader(), $this['prefix']);
+    $this['extension_paths'] = function() {
+      $paths = [];
+      foreach($this['extensions'] as $extension) {
+        $path = drupal_get_path('theme', $extension) ?: drupal_get_path('module', $extension);
+        $paths[] = realpath(sprintf('%s/%s/templates', $this['_drupal_root'], $path));
+      }
+      return $paths;
     };
   }
 
