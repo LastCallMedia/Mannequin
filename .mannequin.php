240871ab3895a97b77fc0d46325d21ee9d6bacbb
Rob Bayliss <rob@lastcallmedia.com>
2017-06-05 09:30:47 -0400

    Update to working .mannequin.php

--- .mannequin.php
+++ .mannequin.php
@@ -3,26 +3,34 @@
 use LastCall\Mannequin\Core\Config;
 use LastCall\Mannequin\Html\Extension\HtmlExtension;
 use LastCall\Mannequin\Twig\Extension\TwigExtension;
+use Symfony\Component\Finder\Finder;
+
+$twigFinder = Finder::create()
+  ->files()
+  ->in(__DIR__.'/twig/Tests/Resources');
+
+$htmlFinder = Finder::create()
+  ->files()
+  ->in(__DIR__.'/html/Tests/Resources');
 
 $twig = new TwigExtension([
-  'paths' => [__DIR__.'/twig/Tests/Resources']
-]);
-$twig->in([
-  __DIR__.'/twig/Tests/Resources',
-]);
-$html = new HtmlExtension();
-$html->in([
-  __DIR__.'/html/Tests/Resources',
+  'finder' => $twigFinder,
+  'twig_paths' => [
+    '__main__' => [__DIR__.'/twig/Tests/Resources']
+  ]
 ]);
 
-$config = Config::create()
+
+$html = new HtmlExtension([
+  'finder' => $htmlFinder,
+]);
+
+$config = Config::create([
+  'styles' => ['https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css'],
+  'scripts' => ['https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/js/foundation.min.js'],
+])
   ->addExtension($html)
   ->addExtension($twig)
-  ->addStyles([
-    'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css'
-  ])
-  ->addScripts([
-    'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/js/foundation.min.js',
-  ]);
+  ->addAssetMapping('assets', __DIR__.'/assets');
 
 return $config;
