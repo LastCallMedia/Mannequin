052b9301929f0b655639a2bf8dd6b73ab87dd874
Rob Bayliss <rob@lastcallmedia.com>
2017-09-21 19:53:43 -0400

    Switch from handlebars to twig for layouts

--- site/metalsmith.config.js
+++ site/metalsmith.config.js
@@ -2,12 +2,11 @@
 import Metalsmith from 'metalsmith';
 import markdown from 'metalsmith-markdown';
 import webpack from 'metalsmith-webpack-2';
-import layouts from 'metalsmith-layouts';
 import ignore from 'metalsmith-ignore';
 import collections from 'metalsmith-collections';
+import twig from 'metalsmith-twig';
 
 var path = require('path');
-var handlebars = require('handlebars');
 
 let metalsmith = Metalsmith(__dirname);
 
@@ -16,26 +15,25 @@ metalsmith.source('./src');
 metalsmith.destination('./dist');
 metalsmith.ignore(['scss', 'layouts']);
 metalsmith.use(ignore(['scss/*', 'js/*', 'layouts/*']));
+// Add a default layout to all markdown files.
+metalsmith.use((files, metalsmith, done) => {
+    Object.keys(files).forEach(file => {
+        if(file.match(/\.md$/) && !files[file].view) {
+            files[file].view = 'default.twig';
+        }
+    });
+    done();
+})
 metalsmith.use(markdown({gfm: true}));
 metalsmith.use(collections({
     Extensions: {pattern: 'extensions/**.md'}
 }));
 
 metalsmith.use(webpack('webpack.config.js', ['./src/js/**', './src/scss/**']));
-metalsmith.use(layouts({
-    engine: 'handlebars',
+metalsmith.use(twig({
     directory: './src/layouts',
-    default: 'default.html',
-    partials: './src/layouts/partials',
     pattern: ['**/*.html'],
-    exposeConsolidate: (consolidateRequires) => {
-        consolidateRequires.handlebars = handlebars;
-        var helpers = require('handlebars-helpers')({
-            handlebars: handlebars
-        });
-    }
 }));
-metalsmith.clean(false);
 
 
 
