225d61b6542993acc97588486e15fc9514a71de5
Rob Bayliss <rob@lastcallmedia.com>
2017-09-21 17:44:06 -0400

    Add plugins

--- site/metalsmith.config.js
+++ site/metalsmith.config.js
@@ -4,6 +4,8 @@ import markdown from 'metalsmith-markdown';
 import webpack from 'metalsmith-webpack-2';
 import layouts from 'metalsmith-layouts';
 import ignore from 'metalsmith-ignore';
+import collections from 'metalsmith-collections';
+
 var path = require('path');
 var handlebars = require('handlebars');
 
@@ -15,14 +17,26 @@ metalsmith.destination('./dist');
 metalsmith.ignore(['scss', 'layouts']);
 metalsmith.use(ignore(['scss/*', 'js/*', 'layouts/*']));
 metalsmith.use(markdown({gfm: true}));
+metalsmith.use(collections({
+    Extensions: {pattern: 'extensions/**.md'}
+}));
+
 metalsmith.use(webpack('webpack.config.js', ['./src/js/**', './src/scss/**']));
 metalsmith.use(layouts({
     engine: 'handlebars',
     directory: './src/layouts',
     default: 'default.html',
     partials: './src/layouts/partials',
-    pattern: ['*.html'],
+    pattern: ['**/*.html'],
+    exposeConsolidate: (consolidateRequires) => {
+        consolidateRequires.handlebars = handlebars;
+        var helpers = require('handlebars-helpers')({
+            handlebars: handlebars
+        });
+    }
 }));
+metalsmith.clean(false);
+
 
 
 
