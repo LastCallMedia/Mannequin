c574f2c9105fbe4b0f5b8e9a6a5cea39263977fc
Rob Bayliss <rob@lastcallmedia.com>
2017-09-21 08:53:42 -0400

    Reconfigure webpack to be able to use CSS images

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -3,48 +3,46 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-    filename: '[name].[contenthash].css',
+    filename: '../css/[name].[contenthash].css',
 });
 
-module.exports = [
-    {
-        entry: {
-            app: [path.resolve('src', 'js', 'main.js')]
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist', 'js'),
-            filename: '[name].[chunkhash].js'
-        },
-        module: {
-            rules: [{
+module.exports = {
+    bail: true,
+    entry: {
+        app: path.resolve('src', 'js', 'main.js'),
+        style: path.resolve('src', 'scss', 'main.scss')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist', 'js'),
+        filename: '[name].[chunkhash].js',
+    },
+    plugins: [extractSass],
+    module: {
+        rules: [
+            {
                 test: /\.js$/,
                 include: [],
                 exclude: [/node_modules/],
                 loader: 'babel-loader'
-            }]
-        }
-    },
-    {
-        entry: {
-            style: [path.resolve('src', 'scss', 'main.scss')]
-        },
-        output: {
-            path: path.resolve(__dirname, 'dist', 'css'),
-            filename: '[name].[chunkhash].css'
-        },
-        module: {
-            rules: [{
+            },
+            {
                 test: /\.scss$/,
                 use: extractSass.extract({
                     use: [
-                        {loader: "css-loader"},
+                        {loader: 'css-loader'},
                         {loader: 'postcss-loader'},
-                        {loader: "sass-loader"},
+                        {loader: 'sass-loader'}
                     ],
                     fallback: 'style-loader'
-                }),
-            }]
-        },
-        plugins: [extractSass]
+                })
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: 'url-loader',
+                options: {
+                    name: '../img/[name].[hash:8].[ext]'
+                }
+            }
+        ]
     }
-];
+};
