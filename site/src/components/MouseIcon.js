0ca105908c3e70f62cf6225568b7a55ebae6a316
Jack Turner <jack@turner.io>
2017-10-27 14:31:29 -0400

    Make Hero Area height browser height. #86
    Next up cross-browser testing…

--- site/src/components/MouseIcon.js
+++ site/src/components/MouseIcon.js
@@ -1,11 +1,31 @@
-
-import React from 'react';
+import React, {Component} from 'react';
 import './MouseIcon.scss';
 
-export default function MouseIcon() {
-    return (
-        <div className="mouse-icon">
-            <div className="wheel"></div>
-        </div>
-    )
+export default class MouseIcon extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {hasScrolled: false};
+        // Bind the handleScroll event to this object, otherwise it can become
+        // dereferenced.
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+    // Handle any scroll event
+    handleScroll(e) {
+        this.setState({hasScrolled: window.scrollY > 0});
+    }
+    // Lifecycle callback to attach the event listener when the component is mounted.
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    // Lifecycle callback to detach the event listener when the component is unmounted.
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+    render() {
+        return (
+            <div className={`mouse-icon${this.state.hasScrolled ? ' fade-out' : ''}`}>
+                <div className="wheel"></div>
+            </div>
+        )
+    }
 }
