65f33be3fe61012cda8a22d6f0eac18ad20ab855
Rob Bayliss <rob@lastcallmedia.com>
2017-11-09 22:34:30 -0500

    (Site) Update sources

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
           },
       },
       {
+          resolve: 'gatsby-source-filesystem',
+          options: {
+              path: `${__dirname}/../src`,
+              name: 'extensions',
+          }
+      },
+      {
           resolve: 'gatsby-transformer-remark',
           options: {
               plugins: [
--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -8,7 +8,12 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     return graphql(`{
     allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___title] }
-      limit: 1000
+      limit: 1000,
+      filter: {
+        fields:{
+          hidden: {ne:true}
+        }
+      }
     ) {
       edges {
         node {
@@ -39,20 +44,72 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
         });
 };
 
+const isReadme = ({relativePath}) => 'README.md' === path.posix.basename(relativePath);
+
 /**
  * Manipulate the URL for nodes as they enter the system.
  */
 exports.onCreateNode = ({node, boundActionCreators, getNode}) => {
     const {createNodeField} = boundActionCreators;
     if(node.internal.type === 'File') {
-        createNodeField({ node, name: `slug`, value: createSlug(node) })
+        // Set up a slug field on the file.  This lets us do alias lookups during markdown
+        // parsing.
+        if(node.sourceInstanceName === 'extensions') {
+            if(isReadme(node)) {
+                createNodeField({ node, name: `slug`, value: createReadmeSlug(node) })
+            }
+            else if(node.relativePath.match('/docs/')) {
+                createNodeField({ node, name: 'slug', value: createExtensionDocSlug(node) })
+            }
+            else {
+                createNodeField({ node, name: `slug`, value: createSlug(node) })
+            }
+        }
+        else {
+            createNodeField({ node, name: `slug`, value: createSlug(node) })
+        }
+
     }
     else if(node.internal.type === 'MarkdownRemark' && getNode(node.parent).internal.type === 'File') {
         const fileNode = getNode(node.parent)
-        createNodeField({ node, name: `slug`, value: createSlug(fileNode) })
+        let hidden = false;
+
+
+        if(fileNode.sourceInstanceName === 'extensions') {
+            if(isReadme(fileNode)) {
+                enrichReadme(node, fileNode, boundActionCreators);
+            }
+            else if(!fileNode.relativePath.match('/docs/')) {
+                // Hide this page.
+                hidden = true;
+            }
+        }
+        // Copy slug field from file.
+        createNodeField({node, name: 'slug', value: fileNode.fields.slug});
+        createNodeField({node, name: 'hidden', value: hidden});
     }
 }
 
-const createSlug = (fileNode) => {
-    return '/' + fileNode.relativePath.replace(/\.md/, '/');
+const createSlug = ({relativePath}) => {
+    return '/' + relativePath.replace(/\.md/, '/');
+}
+
+const createExtensionDocSlug = ({relativePath}) => {
+    let parts = relativePath.replace(/\.md/, '').split('/');
+    parts.splice(1, 1);
+    return `/${parts.join('/')}/`;
+}
+
+/**
+ * README.md acts as the index.
+ */
+const createReadmeSlug = ({relativePath}) => {
+    return `/${path.posix.dirname(relativePath)}/`
+}
+
+const enrichReadme = (markdownNode, fileNode, {createNodeField}) => {
+    const composerFile = `${path.posix.dirname(fileNode.absolutePath)}/composer.json`;
+    const composer = require(composerFile);
+    markdownNode.frontmatter.title = `Mannequin ${fileNode.relativePath.split('/')[0]}`
+    markdownNode.frontmatter.description = composer.description;
 }
