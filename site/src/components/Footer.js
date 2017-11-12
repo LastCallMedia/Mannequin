d955f503736e83f2a8f1d5cf74cbe25518297e94
Rob Bayliss <rob@lastcallmedia.com>
2017-11-12 11:12:52 -0500

    Add newsletter signup form

--- site/src/components/Footer.js
+++ site/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Branding from './Branding'
+import MailchimpForm from 'react-mailchimp-subscribe';
+
 import './Footer.scss';
 
 export default function Footer() {
@@ -13,6 +15,18 @@ export default function Footer() {
         <i className="icon icon-github" />
         <span className="text">Get Support on Github</span>
       </a>
+
+        <MailchimpForm
+            className="Newsletter"
+            action="https://lastcallmedia.us9.list-manage.com/subscribe/post?u=d60cafbefdea2f1ee497ac747&amp;id=3dcf6c9dcc"
+            messages={{
+                inputPlaceholder: 'Subscribe for updates',
+                btnLabel: 'Subscribe',
+                sending: 'Sending...',
+                success: 'Subscribed! Please check your e-mail and follow the confirmation link.',
+                error: 'Oops, we weren\'t able to subscribe you!',
+            }}
+        />
     </footer>
   )
 }
