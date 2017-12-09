e1f0e73919d11b953107bcae4a51536f80a70b91
Jack Turner <jack@turner.io>
2017-12-09 18:33:25 -0500

    (Site) Styling for Mailchimp subscription form #116) (#118)

--- site/src/components/NewsletterSignup.js
+++ site/src/components/NewsletterSignup.js
@@ -1,20 +1,68 @@
-import React from 'react'
-import MailchimpForm from 'react-mailchimp-subscribe'
+import React, { Component } from 'react'
+import jsonp from 'jsonp'
 import './NewsletterSignup.scss'
 
-export default function NewsletterSignup() {
-  return (
-    <MailchimpForm
-      className="NewsletterSignup"
-      action="https://lastcallmedia.us9.list-manage.com/subscribe/post?u=d60cafbefdea2f1ee497ac747&amp;id=3dcf6c9dcc"
-      messages={{
-        inputPlaceholder: 'Subscribe for updates',
-        btnLabel: 'Subscribe',
-        sending: 'Sending...',
-        success:
-          'Subscribed! Please check your e-mail and follow the confirmation link.',
-        error: "Oops, we weren't able to subscribe you!",
-      }}
-    />
-  )
+const action = 'https://lastcallmedia.us9.list-manage.com/subscribe/post?u=d60cafbefdea2f1ee497ac747&amp;id=3dcf6c9dcc'
+const getAjaxUrl = url => url.replace('/post?', '/post-json?')
+
+export default class NewsletterSignup extends Component {
+  constructor(props, ...args) {
+    super(props, ...args)
+    this.state = {
+      status: null
+    }
+  }
+  onSubmit = e => {
+    e.preventDefault()
+    if (!this.input.value || this.input.value.length < 5 || this.input.value.indexOf('@') === -1) {
+      this.setState({
+        status: 'error'
+      })
+      return
+    }
+    const url = getAjaxUrl(action) + `&EMAIL=${encodeURIComponent(this.input.value)}`;
+    this.setState(
+      {
+        status: 'sending'
+      }, () => jsonp(url, {
+        param: 'c'
+      }, (err, data) => {
+        if (err) {
+          this.setState({status: 'error'})
+        } else if (data.result !== 'success') {
+          this.setState({status: 'error'})
+        } else {
+          this.setState({status: 'success'})
+        }
+      })
+    )
+  }
+  render() {
+    const { status } = this.state
+    return (
+      <form className={['NewsletterSignup', status].join(' ')} action={action} method="post" noValidate>
+        <label htmlFor="NewsletterSignup--text-field">Subscribe for updates</label>
+        <div>
+          <input
+            id="NewsletterSignup--text-field"
+            ref={node => (this.input = node)}
+            type="email"
+            defaultValue=""
+            name="EMAIL"
+            required={true}
+            aria-required="true"
+            placeholder='Email'
+          />
+          <button
+            disabled={this.state.status === "sending" || this.state.status === "success"}
+            onClick={this.onSubmit}
+            type="submit"
+          >Subscribe</button>
+        </div>
+        {status === "sending" && <p>Sending…</p>}
+        {status === "success" && <p>Success!</p>}
+        {status === "error" && <p>Enter email before submitting</p>}
+      </form>
+    )
+  }
 }
