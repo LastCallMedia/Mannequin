97bb398c08f89a59e38783accbbd69352923fcf6
Melissa Rossi <mrossi113@gmail.com>
2021-01-05 17:04:13 -0500

    codesniffer

--- src/Core/Exception/ControllerFrozenException.php
+++ src/Core/Exception/ControllerFrozenException.php
@@ -1,9 +1,19 @@
 <?php
 
+/*
+ * This file is part of Mannequin.
+ *
+ * (c) 2017 Last Call Media, Rob Bayliss <rob@lastcallmedia.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace LastCall\Mannequin\Core\Exception;
 
-
+/**
+ * An exception thrown when a frozen contoller is modified.
+ */
 class ControllerFrozenException extends \RuntimeException
 {
 
